421b7fbb0bcd291dec87d479e3ba00f30461479c
Masai <mahapamasai@gmail.com>
2020-04-29 08:43:15 +0200

    show image on prediction page

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import json
 import urllib.request
-
+import os
 from flask import Flask, render_template
 from flask import request
 from fastai.vision import *
 
 app= Flask(__name__)
+
+UPLOADS_FOLDER = os.path.join('static', 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOADS_FOLDER
 
 def main():
     app.run()
@@ -33,11 +36,13 @@ def predict():
 
             pred_class, pred_idx, outputs= learner.predict(img)
 
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+
             save_path=f'static/uploads/{image_name}'
             img.save(save_path)
 
 
-    return render_template('prediction.html', pred_class=pred_class, save_path=save_path)
+    return render_template('prediction.html', pred_class=pred_class, save_path=full_filename)
 
 
 if __name__ == "__main__":
